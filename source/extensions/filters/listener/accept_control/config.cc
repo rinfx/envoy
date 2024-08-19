4a6c08db86f6c312bc0caa88e3af83207739dd73
rinfx <893383980@qq.com>
2024-08-19 17:12:47 +0800

    update

--- source/extensions/filters/listener/accept_control/config.cc
+++ source/extensions/filters/listener/accept_control/config.cc
@@ -8,15 +8,21 @@ namespace ListenerFilters {
 namespace AcceptControl {
 
 Config::Config(const envoy::extensions::filters::listener::accept_control::v3::AcceptControl& config, Stats::Scope& scope)
-    : stats_(generateStats(config.stat_prefix(), scope)), action_(config.action()) {
+  : stats_(generateStats(config.stat_prefix(), scope)), 
+    scope_(scope), 
+    stat_name_pool_(scope.symbolTable()),
+    filter_stat_prefix_(stat_name_pool_.add("accept_control_hit")),
+    action_(config.action()) {
         for (const auto& entry: config.ip_list()) {
-            ip_list_.push_back(Network::Address::CidrRange::create(entry));
+            auto cidr_entry = Network::Address::CidrRange::create(entry);
+            ip_list_.push_back(std::make_pair(cidr_entry, stat_name_pool_.add(cidr_entry.asString())));
         }
     }
 
 bool Config::hitList(const Network::Address::Instance& address) {
     for (const auto &entry: ip_list_) {
-        if(entry.isInRange(address)) {
+        if(entry.first.isInRange(address)) {
+            incCounter({filter_stat_prefix_, entry.second});
             return true;
         }
     }
--- source/extensions/filters/listener/accept_control/config.h
+++ source/extensions/filters/listener/accept_control/config.h
@@ -33,13 +33,21 @@ public:
   const AcceptControlStats& stats() const { return stats_; }
 
 private:
+  void incCounter(const Stats::StatNameVec& names) const {
+    const Stats::SymbolTable::StoragePtr stat_name_storage = scope_.symbolTable().join(names);
+    scope_.counterFromStatName(Stats::StatName(stat_name_storage.get())).inc();
+  }
+
   static AcceptControlStats generateStats(const std::string& prefix, Stats::Scope& scope) {
     return {ALL_ACCEPT_CONTROL_STATS(POOL_COUNTER_PREFIX(scope, prefix))};
   }
 
   const AcceptControlStats stats_;
+  Stats::Scope& scope_;
+  Stats::StatNamePool stat_name_pool_;
+  const Stats::StatName filter_stat_prefix_;
   envoy::extensions::filters::listener::accept_control::v3::AcceptControl::Action action_;
-  std::vector<Network::Address::CidrRange> ip_list_;
+  std::vector<std::pair<Network::Address::CidrRange, Stats::StatName>> ip_list_;
 };
 
 using ConfigSharedPtr = std::shared_ptr<Config>;
